97a3536c5601086b7fa445aa3f2bfb4d75fbd805
Lao <lao@lao.lao>
2023-10-27 19:46:17 +0400

    change example to show how rotation is done

--- examples/generate_sync.py
+++ examples/generate_sync.py
@@ -4,11 +4,17 @@ import random
 
 vercel_ai.logger.setLevel(logging.INFO)
 client = vercel_ai.Client(proxy=True)
-
+model = "openai:gpt-3.5-turbo-16k-0613"
+params = { "maximumLength": 1000 }
 prompt = "What is git?" + " "*random.randint(0, 200)
-params = {
-  "maximumLength": 1000
-}
+done = False
 
-response = client.generate_sync("openai:gpt-3.5-turbo", prompt, params=params)
+while not done:
+  try:
+    response = client.generate_sync("openai:gpt-3.5-turbo", prompt, params=params)
+    done = True
+  except RuntimeError as e:
+    client = vercel_ai.Client()
+
+
 print(response.text)
